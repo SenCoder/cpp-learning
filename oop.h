412fc65af1b1a552ec5bb06ac858e8b16375c9b8
SenCoder <shengyuan@ShengYuan-PC.localdomain>
2020-11-18 18:56:39 +0800

    First commit

---
+++ oop.h
@@ -0,0 +1,10 @@
+//
+// Created by Neo_Y on 2020/11/18.
+//
+
+#ifndef CPP_LEARNING_OOP_H
+#define CPP_LEARNING_OOP_H
+
+void makeStudent(char *name, int age, float score);
+
+#endif //CPP_LEARNING_OOP_H
